a24b1a8067f73c8b80f74d8c902da92f1184a00e
Kweeka <mtalbi29@yahoo.com>
2022-11-01 16:05:36 +0100

    Add project setup files

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['./src/css/styles.scss', './src/js/app.js'],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      title: 'To Do List',
+      template: resolve(__dirname, 'src', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+  ],
+};
