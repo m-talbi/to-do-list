4dab9b476fdba54d819d7c305f21947587055b28
Kweeka <mtalbi29@yahoo.com>
2022-11-02 16:57:31 +0100

    Update app.js, add todo task remove button functionality

--- src/js/app.js
+++ src/js/app.js
@@ -14,36 +14,49 @@ const appendTaskEl = (task) => {
         <input type="checkbox" ${task.isCompleted ? 'checked' : ''} />
         <p class="${task.isCompleted ? 'line_through' : ''}">${task.description}</p>
       </div>
-      <i class="fa-solid fa-ellipsis-vertical draggable options-icon"></i>
+      <div class="task_options">
+        <i class="fa-solid fa-trash-can delete-option-icon"></i>
+        <i class="fa-solid fa-ellipsis-vertical drag-options-icon"></i>
+      </div>
     </article>
   </li>
   `;
   tasksListEl.insertAdjacentHTML('beforeend', taskEl);
 };
 
-const handleTaskCheckBoxClick = (id) => {
-  const taskEl = document.getElementById(`${id}`);
+const removeTaskEl = (taskEl) => {
+  toDoList = toDoList.filter((task) => task.id !== taskEl.id);
+  localStorage.setItem('todo', JSON.stringify(toDoList));
+  taskEl.remove();
+};
+
+const handleTaskBtnsClick = (id) => {
+  const taskEl = tasksListEl.getElementById(`${id}`);
 
   const taskCheckBox = taskEl.querySelector('input');
+  const taskDeleteIcon = taskEl.querySelector('.delete-option-icon');
   const taskDescription = taskEl.querySelector('p');
 
   taskCheckBox.addEventListener('change', () => {
     if (taskCheckBox.checked) {
-      toDoList.find((task) => task.id === parseInt(taskEl.id, 10)).isCompleted = true;
-      localStorage.setItem('todo', JSON.stringify(toDoList));
-      taskDescription.classList.add('line_through');
+      toDoList.find((task) => task.id === taskEl.id).isCompleted = true;
     } else {
-      toDoList.find((task) => task.id === parseInt(taskEl.id, 10)).isCompleted = false;
-      localStorage.setItem('todo', JSON.stringify(toDoList));
-      taskDescription.classList.remove('line_through');
+      toDoList.find((task) => task.id === taskEl.id).isCompleted = false;
     }
+
+    localStorage.setItem('todo', JSON.stringify(toDoList));
+    taskDescription.classList.toggle('line_through');
+  });
+
+  taskDeleteIcon.addEventListener('click', () => {
+    removeTaskEl(taskEl);
   });
 };
 
 const displayToDoList = () => {
   toDoList.forEach((task) => {
     appendTaskEl(task);
-    handleTaskCheckBoxClick(task.id);
+    handleTaskBtnsClick(task.id);
   });
 };
 
@@ -55,9 +68,7 @@ const handleClearBtnClick = () => {
       const taskCheckBox = taskEl.querySelector('input');
       if (!taskCheckBox.checked) return;
 
-      toDoList = toDoList.filter((task) => task.id !== parseInt(taskEl.id, 10));
-      localStorage.setItem('todo', JSON.stringify(toDoList));
-      taskEl.remove();
+      removeTaskEl(taskEl);
     });
   });
 };
@@ -69,15 +80,15 @@ const handleFormSubmit = () => {
     const task = {
       description: form['task-description'].value,
       isCompleted: false,
-      id: toDoList.length === 0 ? 1 : toDoList.at(-1).id + 1,
+      id: Math.random().toString(32).replace(/0\./, ''),
     };
 
     toDoList.push(task);
     localStorage.setItem('todo', JSON.stringify(toDoList));
 
     appendTaskEl(task);
-    handleTaskCheckBoxClick(task.id);
-    addDraggableListener(tasksListEl.lastElementChild);
+    handleTaskBtnsClick(task.id);
+    addDraggableListener(tasksListEl.lastElementChild, toDoList);
     form.reset();
   });
 };
