25a829b6d12006cc54019ebcb92bc9996b6eb564
kweeka <mtalbi29@yahoo.com>
2023-02-05 18:13:03 +0100

    Add sample data for tests

---
+++ src/__mocks__/data.js
@@ -0,0 +1,14 @@
+const mockTaskData = [
+  {
+    description: 'Feed the cats',
+    isCompleted: false,
+    index: 1,
+  },
+  {
+    description: 'Take out the trash',
+    isCompleted: false,
+    index: 2,
+  },
+];
+
+export default mockTaskData;
