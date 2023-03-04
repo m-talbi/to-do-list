683d0da8a3d162e5b169fdd699e486b2f5a8588e
kweeka <mtalbi29@yahoo.com>
2023-03-04 10:02:46 +0100

    Fix bug when deleting an item

--- src/scripts/modules/deleteTask.js
+++ src/scripts/modules/deleteTask.js
@@ -4,7 +4,7 @@ const removeTaskEl = (taskEl, tasksContainerEl, todoList) => {
   const id = taskEl.id.match(/t(\d+)/)[1];
   const item = todoList.find((task) => task.index === parseInt(id, 10));
   tasksContainerEl.querySelector(`#t${item.index}`).remove();
-  todoList.splice(item, 1);
+  todoList.splice(id - 1, 1);
   reOrderTasks(tasksContainerEl, todoList);
 };
 
