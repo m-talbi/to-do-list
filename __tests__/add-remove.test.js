bac2d3488198bf6592b36f9a1015d3288a83b3b1
kweeka <mtalbi29@yahoo.com>
2023-03-04 09:31:51 +0100

    Move tests files outside src directory

---
+++ __tests__/add-remove.test.js
@@ -0,0 +1,67 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import appendTaskEl from '../src/scripts/modules/addTask.js';
+import { saveTaskLocalStorage } from '../src/scripts/modules/utilities.js';
+import removeTaskEl from '../src/scripts/modules/deleteTask.js';
+import TodoListApp from '../src/scripts/modules/app.js';
+import mockTaskData from '../__mocks__/data.js';
+import getAddedTask from '../__mocks__/utility.js';
+
+describe('Add task element <li> to task list <ul>', () => {
+  document.body.innerHTML = `
+      <main>
+        <form action="">
+          <input id="task-description" type="text" placeholder="Add to your list.." required />
+        </form>
+        <ul id="tasksList"></ul>
+      </main>
+      <footer>
+        <input id="clear-list" type="button" value="Clear all completed" />
+      </footer>
+    `;
+
+  const tasksListEl = document.getElementById('tasksList');
+  const form = document.querySelector('form');
+  const clearListBtn = document.getElementById('clear-list');
+  let todoList = [];
+  const app = new TodoListApp(form, tasksListEl, clearListBtn);
+  app.initialize();
+
+  beforeEach(() => {
+    todoList = [];
+    tasksListEl.innerHTML = '';
+  });
+
+  test('Append function should add a new task item', () => {
+    appendTaskEl(mockTaskData[0], tasksListEl);
+    const task = getAddedTask(tasksListEl);
+    expect(task).toEqual(mockTaskData[0]);
+  });
+
+  test('saveTaskLocalStorage function should add new a task item in local storage', () => {
+    saveTaskLocalStorage(mockTaskData[1], todoList);
+    const task = JSON.parse(localStorage.getItem('todo'))[0];
+    expect(task).toEqual(mockTaskData[1]);
+  });
+
+  test('remove function should remove a task item from the DOM', () => {
+    appendTaskEl(mockTaskData[0], tasksListEl);
+    saveTaskLocalStorage(mockTaskData[0], todoList);
+    const taskEl = tasksListEl.lastElementChild;
+    removeTaskEl(taskEl, tasksListEl, todoList);
+    expect(tasksListEl.lastElementChild).toBeNull();
+  });
+
+  test('remove function should remove a task item from local storage', () => {
+    appendTaskEl(mockTaskData[1], tasksListEl);
+    saveTaskLocalStorage(mockTaskData[1], todoList);
+
+    const taskEl = tasksListEl.lastElementChild;
+    removeTaskEl(taskEl, tasksListEl, todoList);
+
+    const task = JSON.parse(localStorage.getItem('todo'))[0];
+    expect(task).toEqual(undefined);
+  });
+});
---
+++ __tests__/update-clear.test.js
@@ -0,0 +1,96 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { editTask, toggleCheckBox } from '../src/scripts/modules/editTask.js';
+import appendTaskEl from '../src/scripts/modules/addTask.js';
+import { saveTaskLocalStorage } from '../src/scripts/modules/utilities.js';
+import TodoListApp from '../src/scripts/modules/app.js';
+import mockTaskData from '../__mocks__/data.js';
+import getAddedTask from '../__mocks__/utility.js';
+
+describe('Update task description', () => {
+  document.body.innerHTML = `
+      <main>
+        <form action="">
+          <input id="task-description" type="text" placeholder="Add to your list.." required />
+        </form>
+        <ul id="tasksList"></ul>
+      </main>
+      <footer>
+        <input id="clear-list" type="button" value="Clear all completed" />
+      </footer>
+    `;
+
+  const tasksListEl = document.getElementById('tasksList');
+  const form = document.querySelector('form');
+  const clearListBtn = document.getElementById('clear-list');
+  let todoList = [];
+  const app = new TodoListApp(form, tasksListEl, clearListBtn);
+  app.initialize();
+
+  beforeEach(() => {
+    todoList = [];
+    localStorage.setItem('todo', '[]');
+    tasksListEl.innerHTML = '';
+  });
+
+  test('Update method should update both the DOM and local storage', () => {
+    appendTaskEl(mockTaskData[0], tasksListEl);
+    saveTaskLocalStorage(mockTaskData[0], todoList);
+
+    const updatedDescription = 'Do my math homework';
+    const taskDescription = tasksListEl.lastElementChild.querySelector('p');
+
+    editTask(todoList, mockTaskData[0].index, updatedDescription, taskDescription);
+
+    const taskFromDOM = getAddedTask(tasksListEl);
+    const taskFromStorage = JSON.parse(localStorage.getItem('todo'))[0];
+
+    expect(taskFromDOM.description).toBe(updatedDescription);
+    expect(taskFromStorage.description).toBe(updatedDescription);
+  });
+
+  test('Clicking on task checkbox should change task completed status', () => {
+    appendTaskEl(mockTaskData[1], tasksListEl);
+    saveTaskLocalStorage(mockTaskData[1], todoList);
+
+    const taskCheckBox = tasksListEl.querySelector('.task_checkbox');
+    const taskDescription = tasksListEl.lastElementChild.querySelector('p');
+
+    taskCheckBox.click();
+    toggleCheckBox(
+      tasksListEl.lastElementChild,
+      taskDescription,
+      taskCheckBox,
+      todoList,
+    );
+
+    const taskFromStorage = JSON.parse(localStorage.getItem('todo'))[0];
+    const taskFromElement = getAddedTask(tasksListEl);
+
+    expect(taskFromElement.isCompleted).toBeTruthy();
+    expect(taskFromStorage.isCompleted).toBeTruthy();
+  });
+
+  test('Clear method should remove a completed task from dom and local storage', () => {
+    mockTaskData.forEach((task) => {
+      appendTaskEl(task, tasksListEl);
+      saveTaskLocalStorage(task, todoList);
+    });
+
+    const tasksCheckBoxes = tasksListEl.querySelectorAll('.task_checkbox');
+
+    tasksCheckBoxes.forEach((checkBox) => {
+      checkBox.click();
+    });
+
+    clearListBtn.click();
+
+    const taskFromStorage = JSON.parse(localStorage.getItem('todo'))[0];
+    const taskFromElement = getAddedTask(tasksListEl);
+
+    expect(taskFromElement).toBeNull();
+    expect(taskFromStorage).toEqual(undefined);
+  });
+});
